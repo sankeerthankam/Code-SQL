a80bf6e24b75d693c5b91d970f8ccc69b34190fb
sankeerthankam <sankeerth.ankam@gmail.com>
2023-12-04 03:27:19 +0530

    Create 17. Select actor_id by name.sql

---
+++ 1. Select/17. Select actor_id by name.sql
@@ -0,0 +1,31 @@
+/*
+Write a query to return GROUCHO WILLIAMS' actor_id.					
+Actor's first_name and last_name are all stored as UPPER case in our database, and the database is case sensitive.					
+					
+Data					
+actor_id	first_name	last_name			
+1	        PENELOPE	GUINESS			
+2	NICK	WAHLBERG			
+3	ED	CHASE			
+4	JENNIFER	DAVIS			
+5	JOHNNY	LOLLOBRIGIDA			
+					
+actor_id	first_name	last_name			
+integer	text	text			
+					
+Output					
+actor_id					
+actor_id1					
+					
+*/
+
+
+SELECT					
+	actor_id				
+FROM					
+	actor				
+WHERE					
+	first_name = 'GROUCHO' AND				
+	last_name = 'WILLIAMS'				
+LIMIT 5					
+;					
