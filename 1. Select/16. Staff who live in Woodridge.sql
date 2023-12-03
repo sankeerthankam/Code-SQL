ebcb2d454c0d3d4568e2df728c8fd24e7b33565e
sankeerthankam <sankeerth.ankam@gmail.com>
2023-12-04 03:24:28 +0530

    Create 16. Staff who live in Woodridge.sql

---
+++ 1. Select/16. Staff who live in Woodridge.sql
@@ -0,0 +1,26 @@
+/*
+Write a query to return the names of the staff who live in the city of 'Woodridge'							
+							
+Data							
+id	name	        address	                phone	      city	      country	  sid
+1	  Mike Hillyer	23 Workhaven Lane		    14033335568	Lethbridge	Canada	  1
+2	  Jon Stephens	1411 Lillydale Drive		6172235589	Woodridge	  Australia	2
+							
+Table Structure							
+id	name	        address	                phone	      city	      country	  sid
+int	text	        text	                  text	      text	      text	    smallint
+							
+Output							
+name							
+name1							
+							
+*/
+
+
+SELECT							
+	name						
+FROM							
+	staff_list						
+WHERE							
+	city = 'Woodridge'						
+LIMIT 5							
