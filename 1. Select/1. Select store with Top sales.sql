b29b78896097d20bb792cf3947f5617692565ee9
sankeerthankam <sankeerth.ankam@gmail.com>
2023-12-04 03:02:43 +0530

    Update 1. Select store with Top sales.sql

--- 1. Select/1. Select store with Top sales.sql
+++ 1. Select/1. Select store with Top sales.sql
@@ -1,11 +1,18 @@
-# Table Structure
-# store	manager	total_sales
-# text	text	numeric
+/* 
+Table Structure
+store	manager	total_sales
+text	text	numeric
   
-# Data						
-# store		manager		total_sales		
-# Woodridge	Jon Stephens	33927.04		
-# Lethbridge	Mike Hillyer	33489.47				
+Data						
+store		manager		total_sales		
+Woodridge	Jon Stephens	33927.04		
+Lethbridge	Mike Hillyer	33489.47				
+
+
+Output
+store		manager
+store1		manager1
+*/	
 
 SELECT 
 	store, manager
