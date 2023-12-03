b49a8497ecb5a1acc829f477c4c49fbd2e7caf28
sankeerthankam <sankeerth.ankam@gmail.com>
2023-12-04 03:03:28 +0530

    Update 2. Select Top 3 Categories by Sales.sql

--- 1. Select/2. Select Top 3 Categories by Sales.sql
+++ 1. Select/2. Select Top 3 Categories by Sales.sql
@@ -2,24 +2,23 @@
 Write a query to find the top 3 film categories that generated the most sales.				
 The order of your results doesn't matter.				
 
-Table Structure
-category  total_sales
-text      decimal
+Table		Structure
+category  	total_sales
+text      	decimal
 
 Data      
 category	total_sales			
-Sports	  5314.21			
+Sports	  	5314.21			
 Classics	3639.59			
-New	      4361.57			
-Family	  4226.07			
-Comedy	  4383.58			
+New	      	4361.57			
+Family	  	4226.07			
+Comedy	  	4383.58			
 				
 Output				
 category				
 Category 1				
 Category 2				
 Category 3				
-				
 */
 
 SELECT 
