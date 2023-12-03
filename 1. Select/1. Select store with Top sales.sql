84b4c60688e691f8a81423e54e8143036f523f37
sankeerthankam <sankeerth.ankam@gmail.com>
2023-12-04 03:10:38 +0530

    Update 1. Select store with Top sales.sql

--- 1. Select/1. Select store with Top sales.sql
+++ 1. Select/1. Select store with Top sales.sql
@@ -1,13 +1,14 @@
 /* 
-Table Structure
-store		manager		total_sales
-text		text		numeric
+Write a query to return the name of the store and its manager, that generated the most sales.
   
 Data						
 store		manager		total_sales		
 Woodridge	Jon Stephens	33927.04		
 Lethbridge	Mike Hillyer	33489.47				
 
+Table Structure
+store		manager		total_sales
+text		text		numeric
 
 Output
 store		manager
